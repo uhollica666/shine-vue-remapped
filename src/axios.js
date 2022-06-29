1fa4d53c20fff9e00171e0b1097decaf04233c87
uhollica666 <ujwal.pradhan666@gmail.com>
2022-06-29 22:50:50 +0600

    baseURL forgot to change

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:8000/api/';
+axios.defaults.baseURL = 'https://test.hemantbhutanrealestate.com/api/';
 axios.defaults.headers.common['Authorization'] = 'Bearer' + localStorage.getItem('token');
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 // axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
